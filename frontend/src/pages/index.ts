c1e9d4e1764fc512a837fd48ad5a46816a2ba3a2
ghost <belikesnab@gmail.com>
2026-01-30 09:09:36 -0500

    added index for pages

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,8 @@
+export { LoginPage } from './LoginPage';
+export { AuthCallbackPage } from './AuthCallbackPage';
+export { DashboardPage } from './DashboardPage';
+export { TeamPage } from './TeamPage';
+export { EventsPage } from './EventsPage';
+export { EventDetailPage } from './EventDetailPage';
+export { AdminPage } from './AdminPage';
+export { AdminEventPage } from './AdminEventPage';
