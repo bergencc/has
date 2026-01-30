4d3d44996b5e09952e5a3de9b04223fa423ae6e8
ghost <belikesnab@gmail.com>
2026-01-30 08:15:15 -0500

    added index for ui component

---
+++ frontend/src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { Button } from './Button';
+export { Input } from './Input';
+export { Card, CardHeader, CardContent, CardFooter } from './Card';
+export { Badge } from './Badge';
+export { Modal } from './Modal';
+export { Loading, LoadingPage, LoadingSpinner } from './Loading';
+export { EmptyState } from './EmptyState';
+export { Alert } from './Alert';
